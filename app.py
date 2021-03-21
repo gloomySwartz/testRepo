28558b3425efbac4ca2c7e1be39518cc6022413b
Łukasz Maćkowiak <lukasz.mackowiak@aiut.com>
2021-03-21 15:12:31 +0100

    Removed test-file, added app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from datetime import datetime as dt
+
+
+def main():
+    print(
+        f'This is a test program. Current time: {dt.now().isoformat()}'
+    )
+
+
+if __name__ == '__main__':
+    main()
