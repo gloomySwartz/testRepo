1b53fc97898dfbbb3e4de476d74889b67c641fa3
Łukasz Maćkowiak <lukasz.mackowiak@aiut.com>
2021-03-21 15:16:33 +0100

    added hello_printer

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from datetime import datetime as dt
+from hello_printer import print_hello
 
 
 def main():
     print(
         f'This is a test program. Current time: {dt.now().isoformat()}'
     )
+    print_hello()
 
 
 if __name__ == '__main__':
