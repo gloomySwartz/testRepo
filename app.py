2d5621a17fc25de8494474e886b470b9bc5b2cab
Tester 01 <test@example.com>
2021-04-11 15:31:44 +0200

    Added function

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from datetime import datetime as dt
 from hello_printer import print_hello
 
 
-def main():
+def another_function():
     print(
         f'This is a test program. Current time: {dt.now().isoformat()}'
     )
+
+
+def main():
+    another_function()
     print_hello()
 
 
